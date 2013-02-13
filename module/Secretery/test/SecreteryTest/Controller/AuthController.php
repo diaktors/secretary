30d1d7e7908d6dbe12b2dcecbcf03f86914ab419
Michael <michael@wesrc.com>
2013-02-13 05:27:36 +0100

    add createDbSchema method to build db schema through setUp method

--- module/Secretery/test/SecreteryTest/Controller/AuthController.php
+++ module/Secretery/test/SecreteryTest/Controller/AuthController.php
@@ -3,15 +3,30 @@
 namespace SecreteryTest\Controller;
 
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Doctrine\ORM\Tools\SchemaTool;
 
 class AuthController extends AbstractHttpControllerTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $em;
+
     public function setUp()
     {
         $this->setApplicationConfig(
             include dirname(__DIR__) . '/../../../../config/test/application.config.php'
         );
         parent::setUp();
+        $this->createDbSchema();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $tool = new SchemaTool($this->em);
+        $tool->dropDatabase();
+        unset($this->em);
     }
 
     protected function injectUserAuth()
@@ -57,6 +72,35 @@ class AuthController extends AbstractHttpControllerTestCase
         $this->getApplicationServiceLocator()->get('servicemanager')
             ->setAllowOverride(true)
             ->setService('BjyAuthorize\Service\Authorize', $BjyAuthorizeMock);
+    }
+
+    protected function createDbSchema()
+    {
+        $this->em = $this->getApplicationServiceLocator()->get('doctrine.entitymanager.ormdefault');
+        $tool     = new SchemaTool($this->em);
+        $classes  = array(
+            $this->em->getClassMetadata('Secretery\Entity\User'),
+            $this->em->getClassMetadata('Secretery\Entity\Role'),
+            $this->em->getClassMetadata('Secretery\Entity\Key'),
+            $this->em->getClassMetadata('Secretery\Entity\Note'),
+            $this->em->getClassMetadata('Secretery\Entity\User2Note'),
+        );
+
+        $tool->createSchema($classes);
+
+        $guestRole = new \Secretery\Entity\Role();
+        $guestRole->setId(1)
+            ->setDefault(true)
+            ->setRoleId('guest');
+
+        $userRole = new \Secretery\Entity\Role();
+        $userRole->setId(2)
+            ->setDefault(false)
+            ->setRoleId('user');
+
+        $this->em->persist($guestRole);
+        $this->em->persist($userRole);
+        $this->em->flush();
     }
 
 }
