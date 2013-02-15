96d1f0f1fcf44a7e302dbfe8248d5c80cb75120f
Michael <michael@wesrc.com>
2013-02-15 05:32:53 +0100

    enable JSONStrategy

--- module/Secretery/config/module.config.php
+++ module/Secretery/config/module.config.php
@@ -255,5 +255,8 @@ return array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
+        'strategies' => array(
+            'ViewJsonStrategy',
+        ),
     ),
 );
