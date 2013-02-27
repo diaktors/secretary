29189e1b3904e33d48cc25aa4f2817a8963ecd02
Michael <michael@wesrc.com>
2013-02-28 00:46:21 +0100

    change doctrine proxy settings
    - see issue #7
    - remove DoctrineOrmModule Prox folder
    - set generate_proxies to false in production
    - change namespace/folder settings

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -15,4 +15,16 @@ return array(
             )
         )
     ),
+    'configuration' => array(
+        'orm_default' => array(
+            'metadata_cache'    => 'array',
+            'query_cache'       => 'array',
+            'result_cache'      => 'array',
+            'driver'            => 'orm_default',
+            'generate_proxies'  => false,
+            'proxy_dir'         => 'data/Secretery/Entity/Proxy',
+            'proxy_namespace'   => 'Secretery\Entity\Proxy',
+            'filters'           => array()
+        )
+    ),
 );
