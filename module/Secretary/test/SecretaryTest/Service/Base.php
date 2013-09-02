4d278170c6cfcb95b9afba58bbb10978619a8f34
Mischosch <michael@sch0ll.de>
2013-09-02 22:22:36 +0200

    add some basic user tests / test setup

---
+++ module/Secretary/test/SecretaryTest/Service/Base.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace SecretaryTest\Service;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+
+class Base extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->setupDoctrine();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        unset($this->em);
+    }
+
+    /**
+     * @return EntityManager
+     */
+    protected function setupDoctrine()
+    {
+        $serviceManager = new \Zend\ServiceManager\ServiceManager(
+            new \Zend\Mvc\Service\ServiceManagerConfig(array())
+        );
+        $serviceManager->setService('ApplicationConfig', $this->getTestConfig());
+        $serviceManager->get('ModuleManager')->loadModules();
+        $serviceManager->get('Application')->bootstrap();
+
+        $this->em = $serviceManager->get('doctrine.entitymanager.orm_default');
+        self::createDbSchema($this->em);
+        return $this->em;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTestConfig()
+    {
+        return include __DIR__ . '/../../../../../config/test/application.config.php';;
+    }
+
+    /**
+     * @param EntityManager $em
+     */
+    static public function createDbSchema(EntityManager $em)
+    {
+        $tool    = new SchemaTool($em);
+        $classes = array(
+            $em->getClassMetadata('Secretary\Entity\User'),
+            $em->getClassMetadata('Secretary\Entity\Role'),
+            $em->getClassMetadata('Secretary\Entity\Key'),
+            $em->getClassMetadata('Secretary\Entity\Note'),
+            $em->getClassMetadata('Secretary\Entity\User2Note'),
+        );
+
+        $tool->createSchema($classes);
+
+        $guestRole = new \Secretary\Entity\Role();
+        $guestRole->setId(1)
+            ->setDefault(true)
+            ->setRoleId('guest');
+
+        $userRole = new \Secretary\Entity\Role();
+        $userRole->setId(2)
+            ->setDefault(false)
+            ->setRoleId('user');
+
+        $keyUserRole = new \Secretary\Entity\Role();
+        $keyUserRole->setId(3)
+            ->setDefault(false)
+            ->setRoleId('keyuser');
+
+        $em->persist($guestRole);
+        $em->persist($userRole);
+        $em->persist($keyUserRole);
+        $em->flush();
+    }
+
+
+
+}
