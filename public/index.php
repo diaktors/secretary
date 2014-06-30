4a8623ed10638fda5d4e4a82d5388fe227b8d0e9
Mischosch <michael@wesrc.com>
2014-07-01 01:54:19 +0200

    enable development mode support

--- public/index.php
+++ public/index.php
@@ -10,5 +10,16 @@ define('REQUEST_MICROTIME', microtime(true));
 // Setup autoloading
 include 'vendor/autoload.php';
 
+if (!defined('APPLICATION_PATH')) {
+    define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
+}
+
+$appConfig = include APPLICATION_PATH . '/config/application.config.php';
+
+if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
+    $appConfig = Zend\Stdlib\ArrayUtils::merge($appConfig, include APPLICATION_PATH . '/config/development.config.php');
+}
+
+
 // Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+Zend\Mvc\Application::init($appConfig)->run();
