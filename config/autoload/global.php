0470bbf35e71667dcafb0f180258f9d543040614
Mischosch <michael@wesrc.com>
2014-07-01 23:43:17 +0200

    change auth setting to basic auth (htpasswd)

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,54 +1,50 @@
 <?php
-
 return array(
     'doctrine' => array(
         'connection' => array(
             'orm_default' => array(
-                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'driverClass' => 'Doctrine\\DBAL\\Driver\\PDOMySql\\Driver',
                 'params' => array(
-                    'host'     => 'localhost',
-                    'port'     => '3306',
-                    'user'     => 'xyz',
-                    'dbname'   => 'wesrc',
-                    'password' => 'abc'
-                )
-            )
+                    'host' => 'localhost',
+                    'port' => '3306',
+                    'user' => 'xyz',
+                    'dbname' => 'wesrc',
+                    'password' => 'abc',
+                ),
+            ),
         ),
         'configuration' => array(
             'orm_default' => array(
-                'metadata_cache'    => 'apc',
-                'query_cache'       => 'apc',
-                'result_cache'      => 'apc',
-                'driver'            => 'orm_default',
-                'generate_proxies'  => false,
-                'proxy_dir'         => 'data/Secretary/Entity/Proxy',
-                'proxy_namespace'   => 'Secretary\Entity\Proxy',
-                'filters'           => array()
-            )
-        )
+                'metadata_cache' => 'apc',
+                'query_cache' => 'apc',
+                'result_cache' => 'apc',
+                'driver' => 'orm_default',
+                'generate_proxies' => false,
+                'proxy_dir' => 'data/Secretary/Entity/Proxy',
+                'proxy_namespace' => 'Secretary\\Entity\\Proxy',
+                'filters' => array(),
+            ),
+        ),
     ),
-
     'translator' => array(
         'locale' => 'en_US',
     ),
-
     'logger' => array(
-        'activate'      => false,
-        'writer'        => 'Stream',
-        'writerOptions' => __DIR__ . '/../../data/log/app.log'
+        'activate' => false,
+        'writer' => 'Stream',
+        'writerOptions' => '/Users/mischosch/Sites/wesrc/secretary/config/autoload/../../data/log/app.log',
     ),
-
     'mail' => array(
-        'activate'      => false,
+        'activate' => false,
         'default_email' => 'xyz@abc.com',
-        'default_from'  => 'noreply@abc.com',
-        'domain_url'    => 'http://abc.com/',
+        'default_from' => 'noreply@abc.com',
+        'domain_url' => 'http://abc.com/',
         'transport' => array(
-            'type'      => 'smtp',
-            'options'   => array(
-                'name'              => 'my localhost',
-                'host'              => 'localhost',
-                'connection_class'  => 'login',
+            'type' => 'smtp',
+            'options' => array(
+                'name' => 'my localhost',
+                'host' => 'localhost',
+                'connection_class' => 'login',
                 'connection_config' => array(
                     'username' => 'xyz@abc.com',
                     'password' => '123',
@@ -56,17 +52,13 @@ return array(
             ),
         ),
     ),
-
     'zf-mvc-auth' => array(
         'authentication' => array(
             'http' => array(
                 'accept_schemes' => array(
-                    0 => 'digest',
+                    0 => 'basic',
                 ),
-                'htdigest' => 'data/htdigest',
-                'realm' => 'Secretary',
-                'digest_domains' => '/api/user /api/note /api/key',
-                'nonce_timeout' => '3600',
+                'realm' => 'SecretaryAPI',
             ),
         ),
     ),
